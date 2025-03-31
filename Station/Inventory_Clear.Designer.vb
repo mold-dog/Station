a40b5b18582e3735c604641e3257b346b898fb15
mold-dog <willa@lcellc.net>
2025-03-31 10:35:37 -0500

    Initial commit.
    Program for assigning the status of 'Shipped' to a part.

---
+++ Station/Inventory_Clear.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Inventory_Clear
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.lbl_end_date = New System.Windows.Forms.Label()
+        Me.lbl_start_date = New System.Windows.Forms.Label()
+        Me.lbl_title = New System.Windows.Forms.Label()
+        Me.dt_picker_end = New System.Windows.Forms.DateTimePicker()
+        Me.dt_picker_start = New System.Windows.Forms.DateTimePicker()
+        Me.lbl_color = New System.Windows.Forms.Label()
+        Me.cmbbox_color = New System.Windows.Forms.ComboBox()
+        Me.lbl_part_type = New System.Windows.Forms.Label()
+        Me.cmbbox_part_type = New System.Windows.Forms.ComboBox()
+        Me.lbl_qty_total = New System.Windows.Forms.Label()
+        Me.lbl_qty_type = New System.Windows.Forms.Label()
+        Me.lbl_qty_color = New System.Windows.Forms.Label()
+        Me.btn_load = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.lbl_narrow_search = New System.Windows.Forms.Label()
+        Me.line_Title = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DGV
+        '
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DGV.DefaultCellStyle = DataGridViewCellStyle1
+        Me.DGV.Location = New System.Drawing.Point(20, 301)
+        Me.DGV.Name = "DGV"
+        Me.DGV.RowTemplate.Height = 28
+        Me.DGV.Size = New System.Drawing.Size(1059, 523)
+        Me.DGV.TabIndex = 3
+        '
+        'lbl_end_date
+        '
+        Me.lbl_end_date.AutoSize = True
+        Me.lbl_end_date.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_end_date.Location = New System.Drawing.Point(659, 55)
+        Me.lbl_end_date.Name = "lbl_end_date"
+        Me.lbl_end_date.Size = New System.Drawing.Size(133, 32)
+        Me.lbl_end_date.TabIndex = 13
+        Me.lbl_end_date.Text = "End Date"
+        '
+        'lbl_start_date
+        '
+        Me.lbl_start_date.AutoSize = True
+        Me.lbl_start_date.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_start_date.Location = New System.Drawing.Point(407, 55)
+        Me.lbl_start_date.Name = "lbl_start_date"
+        Me.lbl_start_date.Size = New System.Drawing.Size(142, 32)
+        Me.lbl_start_date.TabIndex = 12
+        Me.lbl_start_date.Text = "Start Date"
+        '
+        'lbl_title
+        '
+        Me.lbl_title.AutoSize = True
+        Me.lbl_title.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_title.Location = New System.Drawing.Point(30, 19)
+        Me.lbl_title.Name = "lbl_title"
+        Me.lbl_title.Size = New System.Drawing.Size(291, 37)
+        Me.lbl_title.TabIndex = 11
+        Me.lbl_title.Text = "Inventory Cleanup"
+        '
+        'dt_picker_end
+        '
+        Me.dt_picker_end.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.dt_picker_end.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dt_picker_end.Location = New System.Drawing.Point(659, 91)
+        Me.dt_picker_end.Name = "dt_picker_end"
+        Me.dt_picker_end.Size = New System.Drawing.Size(240, 35)
+        Me.dt_picker_end.TabIndex = 10
+        '
+        'dt_picker_start
+        '
+        Me.dt_picker_start.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.dt_picker_start.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dt_picker_start.Location = New System.Drawing.Point(407, 91)
+        Me.dt_picker_start.Name = "dt_picker_start"
+        Me.dt_picker_start.Size = New System.Drawing.Size(240, 35)
+        Me.dt_picker_start.TabIndex = 9
+        '
+        'lbl_color
+        '
+        Me.lbl_color.AutoSize = True
+        Me.lbl_color.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_color.Location = New System.Drawing.Point(301, 202)
+        Me.lbl_color.Name = "lbl_color"
+        Me.lbl_color.Size = New System.Drawing.Size(83, 32)
+        Me.lbl_color.TabIndex = 15
+        Me.lbl_color.Text = "Color"
+        '
+        'cmbbox_color
+        '
+        Me.cmbbox_color.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.cmbbox_color.FormattingEnabled = True
+        Me.cmbbox_color.Location = New System.Drawing.Point(301, 236)
+        Me.cmbbox_color.Name = "cmbbox_color"
+        Me.cmbbox_color.Size = New System.Drawing.Size(297, 28)
+        Me.cmbbox_color.TabIndex = 14
+        '
+        'lbl_part_type
+        '
+        Me.lbl_part_type.AutoSize = True
+        Me.lbl_part_type.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_part_type.Location = New System.Drawing.Point(606, 201)
+        Me.lbl_part_type.Name = "lbl_part_type"
+        Me.lbl_part_type.Size = New System.Drawing.Size(137, 32)
+        Me.lbl_part_type.TabIndex = 17
+        Me.lbl_part_type.Text = "Part Type"
+        '
+        'cmbbox_part_type
+        '
+        Me.cmbbox_part_type.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.cmbbox_part_type.FormattingEnabled = True
+        Me.cmbbox_part_type.Location = New System.Drawing.Point(606, 235)
+        Me.cmbbox_part_type.Name = "cmbbox_part_type"
+        Me.cmbbox_part_type.Size = New System.Drawing.Size(297, 28)
+        Me.cmbbox_part_type.TabIndex = 16
+        '
+        'lbl_qty_total
+        '
+        Me.lbl_qty_total.AutoSize = True
+        Me.lbl_qty_total.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_qty_total.Location = New System.Drawing.Point(30, 91)
+        Me.lbl_qty_total.Name = "lbl_qty_total"
+        Me.lbl_qty_total.Size = New System.Drawing.Size(242, 32)
+        Me.lbl_qty_total.TabIndex = 18
+        Me.lbl_qty_total.Text = "Quantity Affected:"
+        '
+        'lbl_qty_type
+        '
+        Me.lbl_qty_type.AutoSize = True
+        Me.lbl_qty_type.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_qty_type.Location = New System.Drawing.Point(30, 163)
+        Me.lbl_qty_type.Name = "lbl_qty_type"
+        Me.lbl_qty_type.Size = New System.Drawing.Size(212, 32)
+        Me.lbl_qty_type.TabIndex = 20
+        Me.lbl_qty_type.Text = "Types Affected:"
+        '
+        'lbl_qty_color
+        '
+        Me.lbl_qty_color.AutoSize = True
+        Me.lbl_qty_color.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_qty_color.Location = New System.Drawing.Point(30, 235)
+        Me.lbl_qty_color.Name = "lbl_qty_color"
+        Me.lbl_qty_color.Size = New System.Drawing.Size(217, 32)
+        Me.lbl_qty_color.TabIndex = 21
+        Me.lbl_qty_color.Text = "Colors Affected:"
+        '
+        'btn_load
+        '
+        Me.btn_load.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.btn_load.Location = New System.Drawing.Point(914, 78)
+        Me.btn_load.Name = "btn_load"
+        Me.btn_load.Size = New System.Drawing.Size(166, 92)
+        Me.btn_load.TabIndex = 22
+        Me.btn_load.Text = "Load"
+        Me.btn_load.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.btn_delete.Location = New System.Drawing.Point(913, 183)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(166, 92)
+        Me.btn_delete.TabIndex = 23
+        Me.btn_delete.Text = "DELETE"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'lbl_narrow_search
+        '
+        Me.lbl_narrow_search.AutoSize = True
+        Me.lbl_narrow_search.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lbl_narrow_search.Location = New System.Drawing.Point(301, 163)
+        Me.lbl_narrow_search.Name = "lbl_narrow_search"
+        Me.lbl_narrow_search.Size = New System.Drawing.Size(87, 32)
+        Me.lbl_narrow_search.TabIndex = 24
+        Me.lbl_narrow_search.Text = "Filter:"
+        '
+        'line_Title
+        '
+        Me.line_Title.BorderWidth = 3
+        Me.line_Title.Name = "line_Title"
+        Me.line_Title.X1 = 30
+        Me.line_Title.X2 = 330
+        Me.line_Title.Y1 = 56
+        Me.line_Title.Y2 = 56
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.line_Title})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(1101, 839)
+        Me.ShapeContainer1.TabIndex = 25
+        Me.ShapeContainer1.TabStop = False
+        '
+        'Inventory_Clear
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1101, 839)
+        Me.Controls.Add(Me.lbl_narrow_search)
+        Me.Controls.Add(Me.btn_delete)
+        Me.Controls.Add(Me.btn_load)
+        Me.Controls.Add(Me.lbl_qty_color)
+        Me.Controls.Add(Me.lbl_qty_type)
+        Me.Controls.Add(Me.lbl_qty_total)
+        Me.Controls.Add(Me.lbl_part_type)
+        Me.Controls.Add(Me.cmbbox_part_type)
+        Me.Controls.Add(Me.lbl_color)
+        Me.Controls.Add(Me.cmbbox_color)
+        Me.Controls.Add(Me.lbl_end_date)
+        Me.Controls.Add(Me.lbl_start_date)
+        Me.Controls.Add(Me.lbl_title)
+        Me.Controls.Add(Me.dt_picker_end)
+        Me.Controls.Add(Me.dt_picker_start)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Inventory_Clear"
+        Me.Text = "Inventory_Clear"
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DGV As DataGridView
+    Friend WithEvents lbl_end_date As Label
+    Friend WithEvents lbl_start_date As Label
+    Friend WithEvents lbl_title As Label
+    Friend WithEvents dt_picker_end As DateTimePicker
+    Friend WithEvents dt_picker_start As DateTimePicker
+    Friend WithEvents lbl_color As Label
+    Friend WithEvents cmbbox_color As ComboBox
+    Friend WithEvents lbl_part_type As Label
+    Friend WithEvents cmbbox_part_type As ComboBox
+    Friend WithEvents lbl_qty_total As Label
+    Friend WithEvents lbl_qty_type As Label
+    Friend WithEvents lbl_qty_color As Label
+    Friend WithEvents btn_load As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents lbl_narrow_search As Label
+    Friend WithEvents line_Title As PowerPacks.LineShape
+    Friend WithEvents ShapeContainer1 As PowerPacks.ShapeContainer
+End Class
---
+++ Station/Inventory_Clear.vb
@@ -0,0 +1,230 @@
+﻿Public Class Inventory_Clear
+
+    Private bsrc As BindingSource = New BindingSource()
+
+    Private Sub init() Handles MyBase.Load
+
+        dt_picker_start.CustomFormat = "MMMM yyyy"
+        dt_picker_end.CustomFormat = "MMMM yyyy"
+
+        AddHandler cmbbox_part_type.SelectedValueChanged, AddressOf cmbbox_filter
+        AddHandler cmbbox_color.SelectedValueChanged, AddressOf cmbbox_filter
+
+
+        Try
+
+            DGV.AutoGenerateColumns = True
+            DGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+            DGV.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells
+
+
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+
+    Private Sub load_data(sender As Object, e As EventArgs) Handles btn_load.Click
+
+        cmbbox_color.Text = ""
+        cmbbox_part_type.Text = ""
+
+        btn_load.Text = "loading..."
+        btn_load.BackColor = Color.LightYellow
+        Me.Refresh()
+
+        Try
+
+
+            Dim start_time As String = dt_picker_start.Value.Month.ToString & "/1/" & dt_picker_start.Value.Year.ToString & " 12:00:00 AM"
+            Dim end_time As String = dt_picker_end.Value.Month.ToString & "/" & DateTime.DaysInMonth(dt_picker_end.Value.Year, dt_picker_end.Value.Month) & "/" & dt_picker_end.Value.Year.ToString & " 11:59:59 PM"
+            Dim where As String = " Where RFID Like '[a-z]%' and Current_Location not in ((Select id From area where Description = 'Shipped'), (Select id From area where Description = 'Scrap'), (Select id From area where Description = 'Exit')) and Last_Update_Time Between '" & start_time & " ' and '" & end_time & "'"
+
+            Dim count_query As String = "Select (Select Count(RFID) From Part" & where & ") as 'Total', (Select Count(Distinct Part_Type) From Part" & where & ") as 'Part Type', (Select Count(Distinct Color) From Part" & where & ") as 'Color'"
+            Dim cnt As DataSet = toolboxMM.SQLTools.queryDatabase(count_query, "count")
+
+            For Each dr As DataRow In cnt.Tables("count").Rows
+                lbl_qty_total.Text = "Quantity Affected: " & IIf(IsDBNull(dr("Total")), "0", dr("Total").ToString)
+                lbl_qty_type.Text = "Types Affected: " & IIf(IsDBNull(dr("Part Type")), "0", dr("Part Type").ToString)
+                lbl_qty_color.Text = "Colors Affected: " & IIf(IsDBNull(dr("Color")), "0", dr("Color").ToString)
+
+            Next
+
+            Me.Refresh()
+
+
+
+
+            Dim total_query As String = "Select RFID, Part_Type.description as 'Part Type', IIF(paint_Color.Description is null, 'No Color In Database', paint_color.Description) as 'Color', area.description as 'Location' From Part left join area on part.current_location = area.id left join Part_type On part.part_type = part_type.id left join Paint_Color on part.color = Paint_Color.id" & where & " Order By RFID"
+
+            Dim total As DataSet = toolboxMM.SQLTools.queryDatabase(total_query, "total")
+            bsrc.Filter = Nothing
+            bsrc.DataSource = total.Tables("total")
+
+            DGV.DataSource = bsrc.DataSource
+
+            populate_comboboxes(total.Tables("total"))
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message)
+
+        End Try
+
+        btn_load.Text = "Load"
+        btn_load.BackColor = Me.BackColor
+
+
+
+
+    End Sub
+
+
+    Private Sub populate_comboboxes(dtable As DataTable)
+
+        cmbbox_part_type.Items.Clear()
+        cmbbox_color.Items.Clear()
+
+        cmbbox_part_type.Items.Add("")
+        For Each dr As DataRow In dtable.DefaultView.ToTable(True, "Part Type").Rows
+
+            cmbbox_part_type.Items.Add(dr("Part Type"))
+        Next
+
+        cmbbox_color.Items.Add("")
+        For Each dr As DataRow In dtable.DefaultView.ToTable(True, "Color").Rows
+            cmbbox_color.Items.Add(dr("Color"))
+        Next
+
+
+    End Sub
+
+
+
+    Private Sub clear_data(sender As Object, e As EventArgs) Handles btn_delete.Click
+
+
+        Try
+
+            Dim result As DialogResult = MessageBox.Show("Clear displayed parts from inventory?", "Confirm", MessageBoxButtons.YesNo)
+
+            If result = DialogResult.Yes Then
+
+                btn_delete.Text = "deleting..."
+                btn_delete.BackColor = Color.PaleVioletRed
+                btn_delete.Refresh()
+
+
+                Dim start_time As String = dt_picker_start.Value.Month.ToString & "/1/" & dt_picker_start.Value.Year.ToString & " 12:00:00 AM"
+                Dim end_time As String = dt_picker_end.Value.Month.ToString & "/" & DateTime.DaysInMonth(dt_picker_end.Value.Year, dt_picker_end.Value.Month) & "/" & dt_picker_end.Value.Year.ToString & " 11:59:59 PM"
+
+                Dim where As String = " Where RFID Like '[a-z]%' and Current_Location not in ((Select id From area where Description = 'Shipped'), (Select id From area where Description = 'Scrap'), (Select id From area where Description = 'Exit')) and (Last_Update_Time Between '" & start_time & " ' and '" & end_time & "')"
+
+                If cmbbox_part_type.Text <> "" Then
+                    where += " and Part_Type = (Select id from Part_Type Where Description = '" & cmbbox_part_type.Text & "')"
+                End If
+
+                If cmbbox_color.Text <> "" Then
+                    where += " and Color = (Select id from Paint_Color Where Description = '" & cmbbox_color.Text & "')"
+                End If
+
+                ' Dim total_query As String = "Select * From Part" & where
+                Dim update_query As String = "Update Part Set Current_Location = (Select id From area where Description = 'Shipped'), Last_Location = Current_Location" & where
+
+                Dim total As DataSet = toolboxMM.SQLTools.queryDatabase(update_query, "none")
+
+
+                bsrc.DataSource = Nothing
+                bsrc.Filter = Nothing
+                DGV.DataSource = Nothing
+
+
+                btn_delete.Text = "DELETE"
+                btn_delete.BackColor = Me.BackColor
+                btn_delete.Refresh()
+                cmbbox_color.Text = ""
+                cmbbox_part_type.Text = ""
+
+
+            End If
+
+
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub cmbbox_filter(sender As Object, e As EventArgs)
+
+
+        lbl_title.Select()
+
+        btn_load.Text = "loading..."
+        btn_load.BackColor = Color.LightYellow
+        btn_load.Refresh()
+
+
+        Dim start_time As String = dt_picker_start.Value.Month.ToString & "/1/" & dt_picker_start.Value.Year.ToString & " 12:00:00 AM"
+        Dim end_time As String = dt_picker_end.Value.Month.ToString & "/" & DateTime.DaysInMonth(dt_picker_end.Value.Year, dt_picker_end.Value.Month) & "/" & dt_picker_end.Value.Year.ToString & " 11:59:59 PM"
+        Dim where As String = " Where RFID Like '[a-z]%' and Current_Location not in ((Select id From area where Description = 'Shipped'), (Select id From area where Description = 'Scrap'), (Select id From area where Description = 'Exit')) and Last_Update_Time Between '" & start_time & " ' and '" & end_time & "'"
+
+        If cmbbox_part_type.Text <> "" Then
+            where += " and Part_Type = (Select id from Part_Type Where Description = '" & cmbbox_part_type.Text & "')"
+        End If
+
+        If cmbbox_color.Text <> "" Then
+            where += " and Color = (Select id from Paint_Color Where Description = '" & cmbbox_color.Text & "')"
+        End If
+
+        Dim count_query As String = "Select (Select Count(RFID) From Part" & where & ") as 'Total', (Select Count(Distinct Part_Type) From Part" & where & ") as 'Part Type', (Select Count(Distinct Color) From Part" & where & ") as 'Color'"
+        Dim cnt As DataSet = toolboxMM.SQLTools.queryDatabase(count_query, "count")
+
+        For Each dr As DataRow In cnt.Tables("count").Rows
+            lbl_qty_total.Text = "Quantity Affected: " & IIf(IsDBNull(dr("Total")), "0", dr("Total").ToString)
+            lbl_qty_type.Text = "Types Affected: " & IIf(IsDBNull(dr("Part Type")), "0", dr("Part Type").ToString)
+            lbl_qty_color.Text = "Colors Affected: " & IIf(IsDBNull(dr("Color")), "0", dr("Color").ToString)
+
+        Next
+
+        Me.Refresh()
+
+        Dim filter As String = ""
+
+
+
+        If cmbbox_color.Text <> "" Then
+            filter = "color = '" & cmbbox_color.Text & "'"
+
+            If cmbbox_part_type.Text <> "" Then
+                filter += " AND [part type] = '" & cmbbox_part_type.Text & "'"
+            End If
+
+        ElseIf cmbbox_part_type.Text <> "" Then
+            filter = "[part type] = '" & cmbbox_part_type.Text & "'"
+        End If
+
+        If filter = "" Then
+            bsrc.Filter = Nothing
+        Else
+            bsrc.Filter = filter
+        End If
+
+
+
+
+        btn_load.Text = "Load"
+        btn_load.BackColor = Me.BackColor
+
+
+
+    End Sub
+
+
+End Class
